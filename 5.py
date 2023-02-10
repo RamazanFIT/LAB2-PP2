10a1602f2fa997321faf02e8a78b93ad490965b1
Ramazan Syrlybay <116024186+RamazanFIT@users.noreply.github.com>
2023-02-10 13:40:07 +0600

    Update 5.py

--- 5.py
+++ 5.py
@@ -4,6 +4,12 @@ string_input = str(input())
 
 def permut_of_str(string: str):
     string = list(permutations(string))
-    return string 
+    list_1 = []
+    for i in string:
+        s = ""
+        for j in i:
+            s += j 
+        list_1.append(s)
+    return list_1 
 
 print(*permut_of_str(string_input))
