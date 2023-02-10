523730b89eecea91c9f0e42bff3b28c00cefde20
Ramazan Syrlybay <116024186+RamazanFIT@users.noreply.github.com>
2023-02-10 13:47:45 +0600

    Update 11.py

--- 11.py
+++ 11.py
@@ -1,16 +1,9 @@
-string = str(input())
-def ispolindrome(string: str):
-    string_list = list(string)
-    string_list.reverse()
-    prob_string = str()
-    for i in string_list:
-        prob_string += i 
-    if prob_string == string:
+def Ispol(str_1):
+    if str_1 == str_1[::-1]:
         return True 
     else:
-        return False 
+        return False
 
-if ispolindrome(string):
-    print("YES")
-else:
-    print("NO")
+str_1 = str(input())
+
+print(Ispol(str_1))
