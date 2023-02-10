f9fc791600ec973f0ea08bcb8accd987e877ed6b
Ramazan Syrlybay <116024186+RamazanFIT@users.noreply.github.com>
2023-02-10 13:43:01 +0600

    Update 10.py

--- 10.py
+++ 10.py
@@ -1,19 +1,10 @@
-list_1 = list()
-k = int(input())
-for i in range(k):
-    list_1.append(int(input()))
+list_1 = list(map(int, input().split()))
 
-def set_1(list_1: list):
-    list_1.sort()
-    list_1.append("ROMA")
-    list_2 = list()
-    for i in range(len(list_1)-1):
-        boolka = True 
-        if(list_1[i] == list_1[i+1]):
-            boolka = False 
-        else:
-            list_2.append(list_1[i])
-    return list_2
-
+def set_1(list_1):
+    list_2 = []
+    for i in list_1:
+        if i not in list_2:
+            list_2.append(i)
+    return list_2 
 
 print(*set_1(list_1))
